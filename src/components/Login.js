9370a1ff9418e75260c304b5932dc027d1d774cf
Minkang Yang <minkangyang@gmail.com>
2018-12-17 00:20:17 -0500

    finish react class7-login-register-home

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,68 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-    Form, Icon, Input, Button, Checkbox, message,
+  Form, Icon, Input, Button, Checkbox, message,
 } from 'antd';
 import {API_ROOT} from "../constants"
 
 const FormItem = Form.Item;
 
 class NormalLoginForm extends React.Component {
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-                fetch(`${API_ROOT}/login`, {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        username: values.username,
-                        password: values.password
-                    })
-                })
-                .then((response) => {
-                    if (response.ok) {
-                        return response;
-                    }
-                    throw new Error(response.statusText);
-                })
-                .then((response) => {
-                        message.success("Login success");
-                        // TO DO: handle login state change
-                    }
-                )
-                .catch((err) => message.error("Login Fail"))
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        fetch(`${API_ROOT}/login`, {
+          method: 'POST',
+          body: JSON.stringify({
+            username: values.username,
+            password: values.password
+          })
+        })
+          .then((response) => {
+            if (response.ok) {
+              return response.text();
             }
-        });
-    }
+            throw new Error(response.statusText);
+          })
+          .then((response) => {
+              message.success("Login success");
+              this.props.handleLogin(response);
+            }
+          )
+          .catch((err) => message.error("Login Fail"))
+      }
+    });
+  }
 
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <Form onSubmit={this.handleSubmit} className="login-form">
-                <FormItem>
-                    {getFieldDecorator('username', {
-                        rules: [{ required: true, message: 'Please input your username!' }],
-                    })(
-                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                    )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('password', {
-                        rules: [{ required: true, message: 'Please input your Password!' }],
-                    })(
-                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                    )}
-                </FormItem>
-                <FormItem>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        Log in
-                    </Button>
-                    Or <Link to="/register">register now!</Link>
-                </FormItem>
-            </Form>
-        );
-    }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <FormItem>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <Link to="/register">register now!</Link>
+        </FormItem>
+      </Form>
+    );
+  }
 }
 
 export const Login = Form.create()(NormalLoginForm);
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import {Register } from './Register';
-import { Login } from './Login';
 import { Switch, Route, Redirect} from 'react-router-dom';
 
-export class Main extends React.Component {
-    getLogin = () => {
-        return <Redirect to="/login" />
-    }
-    render() {
-        return (
-            <div className="main">
-                <Switch>
-                    <Route exact path={"/"} render={this.getLogin}/>
-                    <Route path={"/register"} component={Register}/>
-                    <Route path={"/login"} component={Login}/>
-                    <Route render={this.getLogin} />
-                </Switch>
-            </div>
+import { Register } from './Register';
+import { Login } from './Login';
+import { Home } from './Home';
 
-        );
-    }
+
+export class Main extends React.Component {
+  getRedirect = () => {
+    return this.props.isLoggedIn ?
+      <Redirect to="/home" /> : <Redirect to="/login" />;
+  }
+
+  getHome = () => {
+    return this.props.isLoggedIn ?
+      <Home/> : <Redirect to="/login"/>;
+  }
+
+  getLogin = () => {
+    return this.props.isLoggedIn ?
+      <Redirect to="/home" /> : <Login handleLogin={this.props.handleLogin} />
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <Switch>
+          <Route exact path={"/"} render={this.getRedirect} />
+          <Route path={"/register"} component={Register} />
+          <Route path={"/login"} render={this.getLogin} />
+          <Route path={"/home"} render={this.getHome} />
+          <Route render={this.getRedirect} />
+        </Switch>
+      </div>
+    );
+  }
 }
